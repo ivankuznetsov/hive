c3e73affcc1f7332959baadf51da5aeea5a61340
Ivan Kuznetsov <ivan@ikuznetsov.com>
2026-05-24 16:20:56 +0100

    test(eval): IU-5 add scenario DSL

--- test/eval/eval_helper.rb
+++ test/eval/eval_helper.rb
@@ -1,3 +1,6 @@
 require "test_helper"
 
-Dir[File.expand_path("support/**/*.rb", __dir__)].sort.each { |path| require path }
+Dir[File.expand_path("support/**/*.rb", __dir__)]
+  .reject { |path| path.end_with?("_test.rb") }
+  .sort
+  .each { |path| require path }

cab271dd0f501ba0a108eb944779f9bcdba3a250
Ivan Kuznetsov <ivan@ikuznetsov.com>
2026-06-06 16:57:48 +0100

    fix(patrol): Leading --json makes hive print help instead of JSON
    (cherry picked from commit 2ecac15178d119c021cced9a8128199576e04c29)
    (cherry picked from commit 93ed654535900088d9f4b09c353a994e283145f9)

--- test/integration/cli_version_test.rb
+++ test/integration/cli_version_test.rb
@@ -1,5 +1,5 @@
 require "test_helper"
-require "open3"
+require "json"
 require "rbconfig"
 
 class CliVersionTest < Minitest::Test
@@ -11,14 +11,18 @@ class CliVersionTest < Minitest::Test
     assert_equal "#{Hive::VERSION}\n", out
   end
 
-  def test_bin_hive_help_after_option_value_shows_command_usage
-    out, err, status = Open3.capture3(
-      RbConfig.ruby, "-Ilib", "bin/hive", "approve", "--from", "2-brainstorm", "--help"
-    )
+  def test_bin_hive_accepts_json_before_status_command
+    with_tmp_global_config do
+      leading = JSON.parse(run!(RbConfig.ruby, "-Ilib", "bin/hive", "--json", "status"))
+      trailing = JSON.parse(run!(RbConfig.ruby, "-Ilib", "bin/hive", "status", "--json"))
 
-    assert status.success?, "bin/hive approve --from 2-brainstorm --help should exit 0, stderr was: #{err}"
-    assert_includes out, "Usage:"
-    assert_includes out, "approve"
-    refute_includes err, "No value provided for required arguments"
+      assert_equal without_generated_at(trailing), without_generated_at(leading)
+    end
+  end
+
+  private
+
+  def without_generated_at(payload)
+    payload.reject { |key, _value| key == "generated_at" }
   end
 end

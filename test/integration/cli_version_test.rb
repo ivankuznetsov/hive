724fbce00277b4d2a2858f9eea8f4810b7ed99bc
Ivan Kuznetsov <ivan@ikuznetsov.com>
2026-06-06 11:51:25 +0100

    fix(patrol): Leading --json is routed as the wrong command
    (cherry picked from commit 7ac83872d10997566274d11e72ec2a21767dde3a)

--- test/integration/cli_version_test.rb
+++ test/integration/cli_version_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require "json"
 require "open3"
 require "rbconfig"
 
@@ -11,29 +12,14 @@ class CliVersionTest < Minitest::Test
     assert_equal "#{Hive::VERSION}\n", out
   end
 
-  def test_bin_hive_help_after_option_value_shows_command_usage
-    out, err, status = Open3.capture3(
-      RbConfig.ruby, "-Ilib", "bin/hive", "approve", "--from", "2-brainstorm", "--help"
-    )
-
-    assert status.success?, "bin/hive approve --from 2-brainstorm --help should exit 0, stderr was: #{err}"
-    assert_includes out, "Usage:"
-    assert_includes out, "approve"
-    refute_includes err, "No value provided for required arguments"
-  end
-
-  def test_bin_hive_accepts_json_before_status_command
+  def test_bin_hive_leading_json_status_keeps_status_command_semantics
     with_tmp_global_config do
-      leading = JSON.parse(run!(RbConfig.ruby, "-Ilib", "bin/hive", "--json", "status"))
-      trailing = JSON.parse(run!(RbConfig.ruby, "-Ilib", "bin/hive", "status", "--json"))
+      out, err, status = Open3.capture3(RbConfig.ruby, "-Ilib", "bin/hive", "--json", "status")
+      assert status.success?, "bin/hive --json status should exit 0, stderr was: #{err}"
 
-      assert_equal without_generated_at(trailing), without_generated_at(leading)
+      payload = JSON.parse(out)
+      assert_equal "hive-status", payload["schema"]
+      assert_equal true, payload["ok"]
     end
-  end
-
-  private
-
-  def without_generated_at(payload)
-    payload.reject { |key, _value| key == "generated_at" }
   end
 end

4e8b9035a9dab2a9e2fafd8381d235c5630199e0
Ivan Kuznetsov <ivan@ikuznetsov.com>
2026-06-07 23:10:46 +0100

    resolve collision: main + PR deliverable

--- test/integration/cli_version_test.rb
+++ test/integration/cli_version_test.rb
@@ -1,5 +1,5 @@
 require "test_helper"
-require "json"
+require "open3"
 require "rbconfig"
 
 class CliVersionTest < Minitest::Test
@@ -9,6 +9,17 @@ class CliVersionTest < Minitest::Test
     out = run!(RbConfig.ruby, "-Ilib", "bin/hive", "--version")
 
     assert_equal "#{Hive::VERSION}\n", out
+  end
+
+  def test_bin_hive_help_after_option_value_shows_command_usage
+    out, err, status = Open3.capture3(
+      RbConfig.ruby, "-Ilib", "bin/hive", "approve", "--from", "2-brainstorm", "--help"
+    )
+
+    assert status.success?, "bin/hive approve --from 2-brainstorm --help should exit 0, stderr was: #{err}"
+    assert_includes out, "Usage:"
+    assert_includes out, "approve"
+    refute_includes err, "No value provided for required arguments"
   end
 
   def test_bin_hive_accepts_json_before_status_command

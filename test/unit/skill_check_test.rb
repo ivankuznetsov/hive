e771699f9e09dc496352a4b17af8e6723b6c9f1a
Ivan Kuznetsov <ivan@ikuznetsov.com>
2026-06-14 01:36:13 +0100

    test: cover pi global npm root discovery

--- test/unit/skill_check_test.rb
+++ test/unit/skill_check_test.rb
@@ -486,6 +486,20 @@ class HiveSkillCheckPiTest < Minitest::Test
     end
   end
 
+  def test_global_npm_root_returns_first_stripped_npm_root_line
+    status = Object.new
+    status.define_singleton_method(:success?) { true }
+    seen_cmd = nil
+
+    with_replaced_singleton_method(Open3, :capture3, lambda { |*cmd|
+      seen_cmd = cmd
+      [ "  /opt/pi/node_modules  \n/ignored\n", "", status ]
+    }) do
+      assert_equal "/opt/pi/node_modules", Hive::SkillCheck::Pi.global_npm_root
+    end
+    assert_equal [ "npm", "root", "-g" ], seen_cmd
+  end
+
   def test_manifest_skill_candidates_expands_jailed_globs
     with_tmp_dir do |package_root|
       write_file("#{package_root}/package.json", '{"pi":{"skills":["custom-*"]}}')

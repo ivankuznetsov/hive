6212f47e4795503b283674c41d03e6861dde88d3
Ivan Kuznetsov <ivan@ikuznetsov.com>
2026-08-13 03:02:30 +0100

    Remove unused attempt proof path

--- lib/hive/paths.rb
+++ lib/hive/paths.rb
@@ -56,10 +56,6 @@ module Hive
       File.join(attempts_root, "generation-locks")
     end
 
-    def attempt_proof_root
-      File.join(attempts_root, "proof")
-    end
-
     def attempt_decision_indexes_root
       File.join(attempts_root, "decision-indexes")
     end

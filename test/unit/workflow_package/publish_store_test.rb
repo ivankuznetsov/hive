2a29c5104cf6d9dafdbf2b63a4b51126ce196819
Ivan Kuznetsov <ivan@ikuznetsov.com>
2026-08-13 17:08:02 +0100

    Remove unused publication secure file wrapper (#1084)

--- test/unit/workflow_package/publish_store_test.rb
+++ test/unit/workflow_package/publish_store_test.rb
@@ -270,10 +270,6 @@ class WorkflowPackagePublishStoreTest < Minitest::Test
         assert_raises(Hive::WorkflowPackage::PublishRecoveryError) do
           store.verify_bundle(receipt)
         end
-        assert_raises(Hive::WorkflowPackage::PublishRecoveryError) do
-          store.send(:secure_file!, File.join(state, "missing-state"))
-        end
-
         assert_equal receipt.identity.transform_keys(&:to_sym), store.send(:symbol_identity, receipt)
       end
     end
